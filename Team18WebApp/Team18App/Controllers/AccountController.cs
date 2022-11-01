f3f5978598cd8b76b6f6643665c8574a7ab669c4
petermendoza <peterjohnm2002@gmail.com>
2022-11-01 14:46:06 -0500

    Added login+register page

---
+++ Team18WebApp/Team18App/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Team18App.Models;
+
+namespace Team18App.Controllers
+{
+    public class AccountController : Controller
+    {
+        Entities entity = new Entities();
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel credentials)
+        {
+            bool userExists = entity.UsersTbls.Any(x => x.Email == credentials.Email && x.Passccode == credentials.Password);
+            UsersTbl u = entity.UsersTbls.FirstOrDefault(x => x.Email == credentials.Email && x.Passccode == credentials.Password);
+            if (userExists)
+            {
+                FormsAuthentication.SetAuthCookie(u.Username, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Username or Password is wrong");
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(UsersTbl userinfo)
+        {
+            entity.UsersTbls.Add(userinfo);
+            entity.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        public ActionResult Signout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Team18WebApp/Team18App/Models/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team18App.Models
+{
+    public static class Roles
+    {
+        public const string EmployeeRole = "EmployeeRole";
+        public const string ClientRole = "ClientRole";
+    }
+}
