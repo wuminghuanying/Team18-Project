779b7fad30a7384406dc5c39f45ce46c36ec1626
Wei Jiang Lin Tan <lintanweiqiang@yahoo.com>
2022-11-02 20:33:06 -0500

    add Controller for each database

---
+++ Team18WebApp/Team18App/Controllers/clientsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Team18App.Models;
+
+namespace Team18App.Controllers
+{
+    public class clientsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: clients
+        public ActionResult Index()
+        {
+            var clients = db.clients.Include(c => c.project);
+            return View(clients.ToList());
+        }
+
+        // GET: clients/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            client client = db.clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // GET: clients/Create
+        public ActionResult Create()
+        {
+            ViewBag.Project_ID = new SelectList(db.projects, "PROJECT_NUM", "project_name");
+            return View();
+        }
+
+        // POST: clients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Fname,Minit,Lname,CLIENT_ID,CLIENT_COMPANY,Project_ID")] client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.clients.Add(client);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Project_ID = new SelectList(db.projects, "PROJECT_NUM", "project_name", client.Project_ID);
+            return View(client);
+        }
+
+        // GET: clients/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            client client = db.clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Project_ID = new SelectList(db.projects, "PROJECT_NUM", "project_name", client.Project_ID);
+            return View(client);
+        }
+
+        // POST: clients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Fname,Minit,Lname,CLIENT_ID,CLIENT_COMPANY,Project_ID")] client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Project_ID = new SelectList(db.projects, "PROJECT_NUM", "project_name", client.Project_ID);
+            return View(client);
+        }
+
+        // GET: clients/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            client client = db.clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            client client = db.clients.Find(id);
+            db.clients.Remove(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
