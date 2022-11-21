96dece66d08b35edda78e9692f5ca9d04b9e3b19
abish <abishekshaju12@gmail.com>
2022-11-20 22:48:51 -0600

    Made report page and form to generate report. Jus

---
+++ Team18WebApp/Team18App/Controllers/TopEmployeesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Team18App.Models;
+
+namespace Team18App.Controllers
+{
+    public class TopEmployeesController : Controller
+    {
+        // GET: TopEmployees
+        int x;
+        int y;
+        int z;
+        Entities entity = new Entities();
+
+        public ActionResult getParamsTE()
+
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult getParamsTE(TopEmployeesFormModel tmp)
+        {
+            if (ModelState.IsValid)
+            {
+                x = tmp.HoursWorked;
+                y = tmp.deptNum; z = tmp.deptNum;
+                return RedirectToAction("getTopEmployees");
+            }
+            return View();
+        }
+        public ActionResult getTopEmployees()
+        {
+            //Entities entity = new Entities();
+            return View(entity.getTopEmployees(x,y,z));
+        }
+
+    }
+}
---
+++ Team18WebApp/Team18App/Models/TopEmployeesFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team18App.Models
+{
+    public class TopEmployeesFormModel
+    {
+        public int HoursWorked { get; set; }
+        public int deptNum { get; set; }
+        public int projNum { get; set; }
+    }
+}
