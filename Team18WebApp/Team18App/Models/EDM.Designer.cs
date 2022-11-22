f0d516eb5be64f4afba94dab058ee112a5ff8e0b
petermendoza <peterjohnm2002@gmail.com>
2022-11-22 12:52:56 -0600

    FINALIZED presentation

--- Team18WebApp/Team18App/Models/EDM.Designer.cs
+++ Team18WebApp/Team18App/Models/EDM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\MySQL Dump Files\Team18\Team18WebApp\Team18App\Models\EDM.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Slaye\Documents\COSC3380 Team 18\Team18WebApp\Team18App\Models\EDM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
