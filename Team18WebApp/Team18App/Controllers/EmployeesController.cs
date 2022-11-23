d6da3e222a494bd6bc66923deb3b9957b01ec9fb
Wei Jiang Lin Tan <lintanweiqiang@yahoo.com>
2022-11-23 15:21:48 -0600

    make all the controller and view

---
+++ Team18WebApp/Team18App/Controllers/EmployeesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Team18App.Models;
+
+namespace Team18App.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private team18dbEntities db = new team18dbEntities();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employees = db.Employees.Include(e => e.User);
+            return View(employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.Users, "id", "userName");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,EmployeeID,Fname,Minit,Lname,DOB,Sex,DepartmentID,Hourly_rate")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.Users, "id", "userName", employee.UserID);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.Users, "id", "userName", employee.UserID);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,EmployeeID,Fname,Minit,Lname,DOB,Sex,DepartmentID,Hourly_rate")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.Users, "id", "userName", employee.UserID);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Team18WebApp/Team18App/Controllers/ProjectsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Team18App.Models;
+
+namespace Team18App.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private team18dbEntities db = new team18dbEntities();
+
+        // GET: Projects
+        public ActionResult Index()
+        {
+            var projects = db.Projects.Include(p => p.Department).Include(p => p.Employee).Include(p => p.Status);
+            return View(projects.ToList());
+        }
+
+        // GET: Projects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjectDept = new SelectList(db.Departments, "DepartmentID", "DepartmentName");
+            ViewBag.ProjectManagerID = new SelectList(db.Employees, "EmployeeID", "Fname");
+            ViewBag.ProjectStatus = new SelectList(db.Status, "StatusID", "StatusName");
+            return View();
+        }
+
+        // POST: Projects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProjectID,ProjectDept,ProjectManagerID,ProjectBudget,CurrentExpenses,ProjectDeadline,ProjectStatus,ProjectName")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjectDept = new SelectList(db.Departments, "DepartmentID", "DepartmentName", project.ProjectDept);
+            ViewBag.ProjectManagerID = new SelectList(db.Employees, "EmployeeID", "Fname", project.ProjectManagerID);
+            ViewBag.ProjectStatus = new SelectList(db.Status, "StatusID", "StatusName", project.ProjectStatus);
+            return View(project);
+        }
+
+        // GET: Projects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectDept = new SelectList(db.Departments, "DepartmentID", "DepartmentName", project.ProjectDept);
+            ViewBag.ProjectManagerID = new SelectList(db.Employees, "EmployeeID", "Fname", project.ProjectManagerID);
+            ViewBag.ProjectStatus = new SelectList(db.Status, "StatusID", "StatusName", project.ProjectStatus);
+            return View(project);
+        }
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProjectID,ProjectDept,ProjectManagerID,ProjectBudget,CurrentExpenses,ProjectDeadline,ProjectStatus,ProjectName")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectDept = new SelectList(db.Departments, "DepartmentID", "DepartmentName", project.ProjectDept);
+            ViewBag.ProjectManagerID = new SelectList(db.Employees, "EmployeeID", "Fname", project.ProjectManagerID);
+            ViewBag.ProjectStatus = new SelectList(db.Status, "StatusID", "StatusName", project.ProjectStatus);
+            return View(project);
+        }
+
+        // GET: Projects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
