4f9469972d2d5c8d793e105546596ebc3697ed87
petermendoza <peterjohnm2002@gmail.com>
2022-11-23 18:53:50 -0600

    Add login

---
+++ Team18WebApp/Team18App/Controllers/AccountsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Team18App.Models;
+using System.Linq;
+
+namespace Team18App.Controllers
+{
+    public class AccountsController : Controller
+    {
+        team18dbEntities entity = new team18dbEntities();
+        // GET: Accounts
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel credentials)
+        {
+            bool userExist = entity.Users.Any(x => x.userName == credentials.Username && x.password == credentials.Password);
+            User u = entity.Users.FirstOrDefault(x => x.userName == credentials.Username && x.password == credentials.Password);
+            if (userExist)
+            {
+                FormsAuthentication.SetAuthCookie(u.userName, false);
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Username or Password is wrong");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User userinfo)
+        {
+            entity.Users.Add(userinfo);
+            entity.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        public ActionResult Signout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+
+
+    }
+}
--- Team18WebApp/Team18App/Models/LoginViewModel.cs
+++ Team18WebApp/Team18App/Models/LoginViewModel.cs
@@ -7,7 +7,7 @@ namespace Team18App.Models
 {
     public class LoginViewModel
     {
-        public string Email { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
     }
 }
